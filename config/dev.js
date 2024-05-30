447d36fc1b1b4784ec6d9a9b9c46455613efdbd1
youssef-khalifa <ykhalifa19921@gmail.com>
2024-05-30 10:53:40 +0300

    first commit

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI:
+    'mongodb+srv://youssefkhalifa:eydCplT4gqYz3hzW@cluster0.m6blbyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    
+  cookieKey: '123123123',
+};
+// user='youssefkhalifa'
+// pass='eydCplT4gqYz3hzW'
